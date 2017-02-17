205fbef0087b18734c1494d7739de363badb6217
Li Xi <lixi@ddn.com>
2017-02-17 16:17:46 +0800

    Add panel for watching the speed of a job
    Job dd.0 will is now watched.
    
    Signed-off-by: Li Xi <lixi@ddn.com>

--- lime_web.py
+++ lime_web.py
@@ -18,6 +18,7 @@ import sys
 from gevent.wsgi import WSGIServer
 from gevent import monkey
 from geventwebsocket.handler import WebSocketHandler
+from geventwebsocket.exceptions import WebSocketError
 
 import utils
 import watched_io
@@ -26,14 +27,136 @@ import lustre_config
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
+class WatchedJobs(object):
+    def __init__(self):
+        self.wjs_jobs = {}
+        self.wjs_condition = threading.Condition()
+
+    def _wjs_find_job(self, job_id):
+        if job_id in self.wjs_jobs:
+            return self.wjs_jobs[job_id]
+        else:
+            return None
+
+    def wjs_find_job(self, job_id):
+        self.wjs_condition.acquire()
+        job = self._wjs_find_job(job_id)
+        self.wjs_condition.release()
+        return job
+
+    def wjs_watch_job(self, job_id, websocket):
+        self.wjs_condition.acquire()
+        job = self._wjs_find_job(job_id)
+        if job is None:
+            job = WatchedJob(job_id, watched_jobs)
+            self.wjs_jobs[job_id] = job
+        job.wj_websockets.append(websocket)
+        self.wjs_condition.release()
+
+    def wjs_unwatch_job(self, job_id, websocket):
+        self.wjs_condition.acquire()
+        job = self._wjs_find_job(job_id)
+        if job is None:
+            self.wjs_condition.release()
+            return -1
+        if websocket in job.wj_websockets:
+            job.wj_websockets.remove(websocket)
+        if len(job.wj_websockets) == 0:
+            del self.wjs_jobs[job_id]
+        self.wjs_condition.release()
+        return 0
+
+    def wjs_metric_received(self, job_id, timestamp, value):
+        self.wjs_condition.acquire()
+        job = self._wjs_find_job(job_id)
+        if job is None:
+            self.wjs_condition.release()
+            return 1
+        dead_websockets = []
+        rate = job.wj_datapoint_add(timestamp, value)
+        console = ("new datapoint, timestamp: %d, value: %d, rate: %d MB/s\n" %
+            (timestamp, value, rate))
+        json_string = json.dumps({"console": console, "rate": rate})
+        for websocket in job.wj_websockets:
+            logging.debug("sending: %s", json_string)
+            try:
+                websocket.send(json_string)
+            except WebSocketError:
+                websocket.closed = True
+                dead_websockets.append(websocket)
+
+        for websocket in dead_websockets:
+            job.wj_websockets.remove(websocket)
+        if len(job.wj_websockets) == 0:
+            del self.wjs_jobs[job_id]
+        self.wjs_condition.release()
+        return 0
+
+class WatchedJob(object):
+    def __init__(self, job_id, jobs):
+        self.wj_websockets = []
+        self.wj_job_id = job_id
+        self.wj_jobs = jobs
+        self.wj_value = None
+        self.wj_timestamp = None
+
+    # Return the rate, if no rate return 0
+    def wj_datapoint_add(self, timestamp, value):
+        if self.wj_timestamp is None:
+            self.wj_timestamp = timestamp
+            self.wj_value = value
+            return 0
+        else:
+            diff = value - self.wj_value
+            time_diff = timestamp - self.wj_timestamp
+            # If diff is negative, return zero
+            rate = 0
+            if diff > 0 and time_diff > 0:
+                rate = diff / time_diff / 1000000
+            self.wj_timestamp = timestamp
+            self.wj_value = value
+            return rate
+
+watched_jobs = WatchedJobs()
+
 @app.route("/")
 def app_root():
     return render_template("index.html")
 
 
+def tsdb_tags_parse(tsdb_tags, tag_dict):
+    tags = tsdb_tags.split()
+    for tag in tags:
+        pair = tag.split("=")
+        if len(pair) != 2:
+            logging.error("tsdb tags [%s] is invalid", tsdb_tags);
+            return -1
+        tag_dict[pair[0]] = pair[1]
+    return 0
+
+
 @app.route("/metric_post", methods=['POST'])
 def app_metric_post():
-    logging.error(json.dumps(request.json, indent=4))
+    logging.debug(json.dumps(request.json, indent=4))
+    for metric in request.json:
+        meta = metric["meta"]
+        tsdb_name = meta["tsdb_name"]
+        if tsdb_name != "ost_jobstats_samples":
+            continue
+        tsdb_tags = meta["tsdb_tags"]
+        tag_dict = {}
+        ret = tsdb_tags_parse(tsdb_tags, tag_dict)
+        if ret:
+            continue
+        logging.debug("tag_dict: %s", tag_dict)
+        if tag_dict["optype"] != "sum_write_bytes":
+            continue
+        logging.debug(json.dumps(metric, indent=4))
+        job_id = tag_dict["job_id"]
+        value = metric["values"][0]
+        timestamp = int(metric["time"])
+        watched_jobs.wjs_metric_received(job_id, timestamp, value)
+        logging.error("time: %d, value: %d", timestamp, value)
     return "Succeeded"
 
 
@@ -43,21 +166,26 @@ def app_console_websocket():
         websocket = request.environ['wsgi.websocket']
         config_string = websocket.receive()
         config = json.loads(config_string)
-        logging.error("received config: %s", config)
+        logging.debug("received config: %s", config)
         fsname = config["name"]
         hosts = []
         for host in config["hosts"]:
             hosts.append(host["name"])
         ssh_identity_file = config["ssh_identity_file"]
-        logging.error("fsname: [%s], hosts: %s", fsname, hosts)
+        logging.debug("fsname: [%s], hosts: %s", fsname, hosts)
         cluster = lustre_config.LustreCluster(fsname, hosts,
             ssh_identity_file=ssh_identity_file)
         cluster.lc_detect_devices()
         cluster.lc_enable_tbf_for_ost_io("nid")
         cluster.lc_set_jobid_var("procname_uid")
-        while True:
+        # TODO: jobid from client
+        job_id = "dd.0"
+        watched_jobs.wjs_watch_job(job_id, websocket)
+        while not websocket.closed:
+            logging.debug("websocket is alive");
             time.sleep(1)
-            websocket.send("looping with config\n")
+        watched_jobs.wjs_unwatch_job(job_id, websocket)
+        logging.error("websocket is closed");
         return "Success"
     else:
         logging.info("run command is not websocket: %s")
--- static/qos.js
+++ static/qos.js
@@ -9,6 +9,8 @@
  */
 
 var QOS = {
+    NAME_PANEL: "panel",
+    ID_PANEL: "panel",
     NAME_CONSOLE_CONTAINER: "console_container",
     ID_CONSOLE_CONTAINER: "#console_container",
     NAME_CONSOLE: "console",
@@ -30,42 +32,110 @@ QoS.prototype.qos_page_init = function()
 
 QoS.prototype.qos_console_init = function()
 {
-    var string = '<div id="' + QOS.NAME_CONSOLE_CONTAINER +
+    if (window.WebSocket == undefined) {
+        console.error("WebSocket is not supported, no console");
+        return
+    }
+
+    var string = '<div id="' + QOS.NAME_PANEL + '" class="panel"></div>';
+    $(string).appendTo("#content");
+    var mychart = echarts.init(document.getElementById(QOS.NAME_PANEL));
+    var option = {
+        series: [
+            {
+                name: 'Write Performance',
+                min: 0,
+                max: 1000,
+                splitNumber:10,
+                type: 'gauge',
+                detail: {formatter:'{value}'},
+                data: [{value: 0, name: 'MB/s'}],
+
+                axisLine: {
+                    lineStyle: {
+                        color: [[0.09, 'lime'],[0.82, '#1e90ff'],[1, '#ff4500']],
+                        width: 3,
+                        shadowBlur: 10
+                    }
+                },
+
+                axisLabel: {
+                    textStyle: {
+                        fontSize: 8,
+                    }
+                },
+
+                axisTick: {
+                    length :10,
+                    lineStyle: {
+                        color: 'auto',
+                    }
+                },
+                splitLine: {
+                    length :20,
+                    lineStyle: {
+                        color: 'auto',
+                    }
+                },
+                title : {
+                    textStyle: {
+                        fontSize: 10,
+                    }
+                },
+                detail : {
+                    textStyle: {
+                        fontWeight: 'bolder',
+                        fontSize: 10,
+                    }
+                },
+            }
+        ]
+    };
+
+    string = '<div id="' + QOS.NAME_CONSOLE_CONTAINER +
         '" class="console_container"></div>';
     $(string).appendTo("#content");
-    if(window.WebSocket != undefined) {
-        string = '<pre id="' + QOS.NAME_CONSOLE +
-            '" class="console"></pre>';
-        $(string).appendTo(QOS.ID_CONSOLE_CONTAINER);
-        var data_string = JSON.stringify(
-            this.qos_lime.l_control_table.ct_config,
-            null, 4);
 
-        var ws_url = 'ws://'+ window.location.hostname +
-            (window.location.port ? ':' + window.location.port : '') +
-            '/console_websocket';
+    string = '<pre id="' + QOS.NAME_CONSOLE +
+        '" class="console"></pre>';
+    $(string).appendTo(QOS.ID_CONSOLE_CONTAINER);
+    var data_string = JSON.stringify(
+        this.qos_lime.l_control_table.ct_config,
+        null, 4);
 
-        var websocket = new WebSocket(ws_url);
-        var workspace = this.rc_result_title;
-        websocket.onopen = function(evt) {
-            websocket.send(data_string);
-        };
-        websocket.onclose = function(evt) {
-        };
-        websocket.onmessage = function(evt) {
-            var string = $(QOS.ID_CONSOLE).text() + evt.data;
-            $(QOS.ID_CONSOLE).text(string);
-            $(QOS.ID_CONSOLE_CONTAINER).scrollTop($(QOS.ID_CONSOLE_CONTAINER)[0].scrollHeight);
-        };
-        websocket.onerror = function(evt) {
-            console.log("onerror");
-        };
-    } else {
-        console.error("WebSocket is not supported, no console");
-    }
+    var ws_url = 'ws://'+ window.location.hostname +
+        (window.location.port ? ':' + window.location.port : '') +
+        '/console_websocket';
+
+    var websocket = new WebSocket(ws_url);
+    var workspace = this.rc_result_title;
+    websocket.onopen = function(evt) {
+        websocket.send(data_string);
+    };
+    websocket.onclose = function(evt) {
+    };
+    websocket.onmessage = function(evt) {
+        var message = JSON.parse(evt.data);
+        var console_message = message.console
+        var string = $(QOS.ID_CONSOLE).text() + console_message;
+        var rate = message.rate
+        $(QOS.ID_CONSOLE).text(string);
+        $(QOS.ID_CONSOLE_CONTAINER).scrollTop($(QOS.ID_CONSOLE_CONTAINER)[0].scrollHeight);
+        option.series[0].data[0].value = rate;
+        mychart.setOption(option, true);
+    };
+    websocket.onerror = function(evt) {
+        console.log("onerror");
+    };
+    /*
+    setInterval(function () {
+        option.series[0].data[0].value = (Math.random() * 1000).toFixed(2) - 0;
+        mychart.setOption(option, true);
+    },2000);
+    */
 }
 
 QoS.prototype.qos_page_fini = function()
 {
-    $(QOS.ID_CONSOLE).remove();
+    $(QOS.ID_CONSOLE_CONTAINER).remove();
 }
